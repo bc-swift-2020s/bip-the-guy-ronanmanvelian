74291b5b3ec8d6dd31e1702b790f6c715311036b
Ronan Manvelian <manvelia@bc.edu>
2020-02-03 01:39:55 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Ronan Manvelian on 2/3/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
